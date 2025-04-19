c99c8922cc811a540dafed7d364a663d7978a5ec
sidxdhiman <sidharthdhimangurgaon@gmail.com>
2025-04-19 13:42:47 +0530

    axios update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "https://craaaackle-backend.onrender.com/api"
-      : import.meta.env.VITE_BACKEND_URL || "/api", 
+      ? "https://craaaackle-backend.onrender.com/"
+      : import.meta.env.VITE_BACKEND_URL || "/", 
   withCredentials: true,
 });
